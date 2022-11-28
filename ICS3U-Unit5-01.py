da4efd8aa8616f9a0014ad9b52891d6f6c54a5c2
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-28 05:04:37 +0000

    code has bin edited

---
+++ ICS3U-Unit5-01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Created by: Emmanuel
+# Created on: Nov 2022
+# This program uses user defined functions
+
+import math
+
+
+def fahrenheit():
+    # this function takes temp in degrease celsius 
+    # and converts it into fahrenheit.
+
+    # input
+    celsius_string = input("Enter a temperature (°C): ")
+
+    try:
+        print("")
+        celsius_temp = float(celsius_string)
+        fahrenheit_temp = 9 / 5 * celsius_temp + 32
+        print("{0}°C is equal to {1:.2f}°F.".format(celsius_temp, fahrenheit_temp))
+
+    except ValueError:
+        print("{0}, is invalid input please try again.")
+
+    print("\n\nDone.")
+
+
+def main():
+    fahrenheit()
+
+    
+if __name__ == "__main__":
+    main()
